156ac15862e479267c4187146b1ddb6389580b26
Philip Ingram <neodestro734@yahoo.com>
2014-04-18 16:49:39 -0700

    Searchable module with where

--- lib/active_record_lite/03_searchable.rb
+++ lib/active_record_lite/03_searchable.rb
@@ -3,10 +3,23 @@ require_relative '02_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+		table_n = self.table_name
+    where_string = params.keys.map(&:to_s).join(" = ? AND ") + " = ?"
+    where_values = params.values
+
+    query = <<-SQL
+      SELECT
+        *
+      FROM
+      	#{table_n}
+      WHERE
+        #{where_string}
+    SQL
+
+    self.parse_all(DBConnection.instance.execute(query, where_values))
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
